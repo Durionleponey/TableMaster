b6d9aef1574731409edcc7eb44497c444e20ec4a
Simon <113543413+Mister-DS@users.noreply.github.com>
2024-10-17 20:58:04 +0200

    Update interface.py
    J'ai changé avec une version et le frame pour garder la même fenêtre (Sims)

--- interface.py
+++ interface.py
@@ -1,11 +1,12 @@
+
 #Permet de tout importer dans tkinter
 from tkinter import *
 from PIL import Image, ImageTk
+from imaplib import *
 
 
 
-
-#Créer une feneter 
+#Créer une feneter
 fenetre = Tk()
 #régler la taille de la fenêtre (1x2) 1 = longueur, 2 =largeur
 fenetre.geometry('600x600')
@@ -13,100 +14,30 @@ fenetre.geometry('600x600')
 fenetre.title("Mon application")
 #Changer la couleur du bg de la fenetre
 fenetre['bg'] = 'cyan'
-#empecher la redimension de la feneter 
+#empecher la redimension de la feneter
 fenetre.resizable(height=False, width=False)
 
 
-# #Créer une zone de texte ------------------------
-
-# label = Label(fenetre, text='Je fais des testes', font=('Helvetica', 20, 'bold italic'), fg="red", bg='cyan')
-# afficher le label 
-# label.pack()#side coter et padx nombre de pixel
-# label.place(x='180', y='160')
-# Mettre du texte en dessous 
-
-# sub_label = Label(fenetre, text="J'aime python", font=('Helvetica', 20, 'bold italic'), fg="red", bg='cyan')
-# #Change le texte grace à un input
-# change_texte = input("Changer le texte : ")
-# sub_label['text'] = change_texte
-# sub_label.pack()
-
-#Les boutons ----------------------------------
-
-# def dire_bjr():
-#     print("Bonjour !")
-
-# bouton = Button(fenetre, text="Click me !", bg="red", fg='white', command=dire_bjr)
-# bouton.pack()
-
-#Les interfaces utilisateurs ----------------
-
-# def function():
-#     label["text"] = ma_var.get()
-
-# ma_var = StringVar()
-# label = Label(fenetre, text="Texte modifiable") 
-# label.pack()
-# entree = Entry(fenetre, bg="white", textvariable=ma_var)
-# entree.pack()
-# bouton = Button(fenetre, bg="white", command=function, text="Valider")
-# bouton.pack()
-
-#Interface Graphique --------------------
-
-# mon_menu = Menu(fenetre)
-
-# def save():
-#     print(f"Vous avez cliqué sue le sous onglet fichier")
-# def options():
-#     print(f"Vous avez cliqué sue le sous onglet option")
-
-# #Sous onglet Fichier
-
-# fichier = Menu(mon_menu, tearoff=0)
-# fichier.add_command(label="Enregistrer sous...", command=save)
-
-# #Sous onglet Option
-
-# option = Menu(mon_menu, tearoff=0)
-# option.add_command(label="Modifier la taille", command=options)
-
-# #Les 2 principaux onglets
-# mon_menu.add_cascade(label="fichier", menu=fichier)
-# mon_menu.add_cascade(label='Option',  menu=option)
 
 
-# fenetre.config(menu=mon_menu)
-
-#Les boites ---------------------
-
-# boite = Frame(fenetre, bg="green")
-
-# label = Label(boite, text="Mon premier texte")
-# label.pack()
-# label2 = Label(boite, text="Mon 2 eme texte")
-# label2.pack()
-
-# boite.pack(expand=YES)
-
-
-
-#Afficher des images --------------------
-
-# photo = PhotoImage(file='photo.jpg')
-# label = Label(fenetre, image=photo)
-# label.pack()
 
 
 
 #Image clickable  ---------------------
 
 def image_click():
-    print("Vous avez cliqué sur l'image")
+    fenetre2 = Tk()
+    fenetre2.geometry('400x400')
+    fenetre2.title('Réservation')
+    fenetre2['bg'] = 'lightgreen'
+    fenetre2.resizable(height=True, width=True)
+
+    label = Label(fenetre2, text='Trop stylaxe')
+    label.pack()
 
 
-image = Image.open('C:\\Users\\tiago\\Desktop\\ok\\table.jpg')
-image = image.resize((300, 300)) 
+image = Image.open('img\\table_restau.jpeg')
+image = image.resize((70, 70))
 photo = ImageTk.PhotoImage(image)
 
 
@@ -114,5 +45,70 @@ photo = ImageTk.PhotoImage(image)
 image_bouton = Button(fenetre, image=photo, command=image_click)
 image_bouton.pack()
 
-#Appeler la feneter 
+#Appeler la feneter
 fenetre.mainloop()
+
+
+
+
+
+
+"""
+Fausse version : 
+
+# Importer les modules nécessaires
+from tkinter import *
+from PIL import Image, ImageTk  # Importer les modules pour gérer les images avec Pillow
+
+# Fonction pour basculer vers une nouvelle interface
+def switch_frame(new_frame):
+    # Masquer l'ancienne interface
+    current_frame.pack_forget()
+    # Afficher la nouvelle interface
+    new_frame.pack(fill='both', expand=True)
+
+# Créer une fenêtre
+fenetre = Tk()
+fenetre.geometry('600x600')
+fenetre.title("Mon application")
+fenetre['bg'] = 'cyan'
+fenetre.resizable(height=False, width=False)
+
+# Interface 1 : Afficher une image et un bouton
+current_frame = Frame(fenetre)
+current_frame.pack(fill='both', expand=True)
+
+# Charger et redimensionner l'image avec Pillow
+try:
+    image = Image.open('img\\table_restau.jpeg')
+    image = image.resize((300, 300))  # Redimensionner l'image
+    photo = ImageTk.PhotoImage(image)  # Convertir l'image pour Tkinter
+except FileNotFoundError:
+    print("L'image n'a pas été trouvée. Vérifiez le chemin du fichier.")
+    photo = None
+
+# Afficher l'image si elle est chargée
+if photo:
+    image_label = Label(current_frame, image=photo)
+    image_label.pack()
+
+# Interface 2 : Nouvelle interface après le clic
+new_frame = Frame(fenetre)
+
+# Ajouter du contenu à la nouvelle interface
+new_label = Label(new_frame, text="Vous êtes sur la nouvelle interface !", font=('Helvetica', 16))
+new_label.pack(pady=20)
+
+# Bouton pour retourner à l'interface d'origine
+back_button = Button(new_frame, text="Retour", command=lambda: switch_frame(current_frame))
+back_button.pack(pady=20)
+
+# Clic sur l'image pour basculer vers la nouvelle interface
+if photo:
+    image_bouton = Button(current_frame, text="Cliquez ici !", command=lambda: switch_frame(new_frame))
+    image_bouton.pack()
+
+# Lancer la boucle principale de la fenêtre
+fenetre.mainloop()
+"""
+
