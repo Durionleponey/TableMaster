184f726ed7dcbef03a982072b887f5fba0a582af
Simon <113543413+Mister-DS@users.noreply.github.com>
2024-10-19 17:17:21 +0200

    Ajout des vérifications pour les labels (simon)

--- formulaire_reservation.py
+++ formulaire_reservation.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+import re
 
 # Créer une fenêtre
 formu = Tk()
@@ -11,9 +12,141 @@ formu.resizable(height=False, width=False)
 # Différentes fonctions :
 def register_client():
     register_nom = zone_nom.get()
+
+    #Vérification remplissage de case :
+    if register_nom == "":
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, vous n'avez pas rempli la case du Nom", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
+
+    elif register_nom.isalpha() == False:
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, vous avez entrer un numéro dans la case du Nom", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
+
     register_prenom = zone_prenom.get()
+
+    if register_prenom == "":
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, vous n'avez pas rempli la case du prénom", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
+
+    elif register_prenom.isalpha() == False:
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, vous avez entrer un numéro dans la case du prénom", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
+
+
     register_nbre = zone_nombre.get()
+
+    #Vérification c'est un nombre
+
+    if register_nbre =="":
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, vous n'avez pas rempli la case du nombres de personnes présentes", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
+
+    #Vérification nombre et entier :
+    elif register_nbre.isdigit() == False:
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, le chiffre que vous avez rentrer n'est pas un entier !", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
+
+#Vérifier si la zone n'est pas vide :
+
     register_heure = zone_heure.get()
+    if register_heure =="":
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, vous n'avez pas rempli la case du nombres de personnes présentes", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
+
+    # Vérifier si c'est bien un format d'heure (HH:MM)
+    pattern_heure = r"^([01]\d|2[0-3]):([0-5]\d)$"
+
+    if not re.match(pattern_heure, register_heure):
+        fenetre_erreur = Tk()
+        fenetre_erreur.geometry('300x150')
+        fenetre_erreur.title("Erreur")
+        fenetre_erreur.configure(bg='lightgrey')
+        fenetre_erreur.resizable(height=False, width=False)
+
+        error_mess = Label(fenetre_erreur, text="Error, l'heure que vous avez entrée n'est pas valide !", bg='Red')
+        error_mess.pack()
+
+        # Bouton pour fermer la fenêtre d'erreur
+        close_button = Button(fenetre_erreur, text="Revenir au formulaire", bg='red', fg='white',
+                              command=fenetre_erreur.destroy)
+        close_button.pack(pady=10)
 
     print(f"Vous êtes {register_nom} {register_prenom}, vous serez {register_nbre} et vous avez réservé à {register_heure}.")
 
@@ -59,3 +192,6 @@ validation.grid(row=5, column=0, columnspan=2, pady=20)
 
 # Lancement de la fenêtre
 formu.mainloop()
+
+
+if __name__ = "__main__"
